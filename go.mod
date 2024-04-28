508d505a7205a630029e303542cdf80f0aa5211b
ProggerX <x@proggers.ru>
2024-04-28 14:24:36 +0300

    Initial commit
     Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ProggerX/homepage
+
+go 1.22.2
+
+require (
+	github.com/briandowns/openweathermap v0.19.0
+	github.com/joho/godotenv v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import "html/template"
+import "net/http"
+import "time"
+import "fmt"
+import "github.com/joho/godotenv"
+import owm "github.com/briandowns/openweathermap"
+import "os"
+
+type Button struct {
+	Name string
+	Icon string
+	Link string
+}
+
+type Homepage struct {
+	Time string
+	Buttons []Button
+	Weather string
+}
+
+func getWeather() string {
+	godotenv.Load()
+	var apiKey = os.Getenv("OWM_API_KEY")
+	wtr, err := owm.NewCurrent("C", "ru", apiKey)
+	if err != nil {
+		panic(err)
+    }
+	wtr.CurrentByName("Moscow")
+	return fmt.Sprintf("%d°C", int(wtr.Main.Temp))
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	tmpl, _ := template.ParseFiles("templates/main.html")
+	tmpl.Execute(w, Homepage{
+		Time: fmt.Sprintf("%02d:%02d", time.Now().Hour(), time.Now().Minute()),
+		Buttons: []Button{
+			{"GitHub", "", "https://github.com/"},
+			{"Google", "", "https://google.com/"},
+			{"YouTube", "󰗃", "https://youtube.com/"},
+			{"Music", "󰝚", "https://music.yandex.ru/"},
+		},
+		Weather: getWeather(),
+	})
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", handler)
+
+	http.ListenAndServe(":8008", mux)
+}
