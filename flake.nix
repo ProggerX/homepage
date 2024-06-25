c2dc7284c1e82782d1207b45cefa6d88bfd946e6
ProggerX <x@proggers.ru>
2024-06-25 11:25:49 +0300

    Added flake

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	};
+
+	outputs = { self, nixpkgs, ... }:
+	let system = "aarch64-linux";
+	pkgs = nixpkgs.legacyPackages.${system};
+	in {
+		packages."${system}".default = pkgs.stdenv.mkDerivation {
+			name = "homepage";
+			src = ./.;
+			nativeBuildInputs = with pkgs; [
+				go
+			];
+			buildPhase = ''go build .'';
+			installPhase = ''mkdir $out && cp -r ./* $out'';
+		};
+		nixosModules.site = { config, lib, ... }: {
+			options = {
+				server.homepage.enable = lib.mkEnableOption "Enable ProggerX's homepage";
+			};
+			config = lib.mkIf config.server.site.enable {
+				systemd.services.homepage = {
+					wantedBy = [ "multi-user.target" ];
+					serviceConfig = {
+						WorkingDirectory = "${self.packages."${system}".default}";
+						ExecStart = "${self.packages."${system}".default}/homepage";
+					};
+				};
+				services.nginx = {
+					enable = true;
+					virtualHosts.site = {
+						addSSL = true;
+						enableACME = true;
+						serverName = "home.bald.su";
+						locations."/" = {
+							proxyPass = "http://0.0.0.0:8008";
+						};
+					};
+				};
+				security.acme = {
+					acceptTerms = true;
+					defaults.email = "x@proggers.ru";
+				};
+				networking.firewall.allowedTCPPorts = [ 80 443 ];
+			};
+		};
+	};
+}
